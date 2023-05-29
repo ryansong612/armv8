4a81ab428413d7a01a6f34608e068f0529a0be0d
ryansong612 <ryansong612@gmail.com>
2023-05-29 15:48:30 +0100

    Register Structs Defined

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,44 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+
+// filepath: /Users/ryansong612/Desktop/ICL/Courses/COMP40009/ARMv8-page29-files/bootcode.bin
+
+typedef struct {
+    char mode; // Either 'x' for 64, or 'w' for 32
+    unsigned int id; // id of register (0-30), if out of range, it is special
+    unsigned char bits[64]; // bits stored in the register
+    bool zeroRegisterFlag; // 1 if it is a zero register
+    bool programCounterFlag; // 1 if it is a program counter
+} GeneralPurposeRegister;
+
+typedef struct {
+    bool negativeConditionFlag;
+    bool zeroConditionFlag;
+    bool carryConditionFlag;
+    bool overflowConditionFlag;
+} PSTATE;
+
+
+void readFile(char *dst) {
+    unsigned char buffer[34];
+    FILE *ptr;
+
+    ptr = fopen(dst, "rb");
+    fread(buffer, sizeof(buffer), 1, ptr);
+
+    for (int i = 0, n = sizeof(buffer); i < n; i++) {
+        printf("%u ", buffer[i]);
+    }
+
+    printf("\n");
+
+    fclose(ptr);
+}
+
 
 int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
+    readFile(argv[1]);
+    return 1;
 }
