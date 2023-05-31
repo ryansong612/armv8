8ee8906f91a3348bfc176cb36c6df7e81e1e64af
imcoolthanks <wongq9999@outlook.com>
2023-05-31 15:46:51 +0100

    changed my functions to return booleans

--- src/parseLS.c
+++ src/parseLS.c
@@ -12,7 +12,7 @@ extern uint64_t programCounter;
 extern PSTATE pStateRegister;
 extern uint8_t memory[MAX_FILE_SIZE];
 
-void load_literal(uint32_t instruction, GeneralPurposeRegister *targetRegister, int numBytesToAccess) {
+bool load_literal(uint32_t instruction, GeneralPurposeRegister *targetRegister, int numBytesToAccess) {
     // gets address PC + simms19 * 4
     int32_t simms19 = get_bits(instruction, 5, 23) >> 5;
     int64_t offset = extendSignBit(simms19 * 4, 32); // sign extend it
@@ -27,18 +27,32 @@ void load_literal(uint32_t instruction, GeneralPurposeRegister *targetRegister, 
 
     // writes this to the target register
     (*targetRegister).val = value;
+    return true;
 }
 
-void unsigned_immediate_offset(uint32_t instruction, GeneralPurposeRegister *targetRegister, int8_t xn, int numBytesToAccess) {
+bool unsigned_immediate_offset(uint32_t instruction, GeneralPurposeRegister *targetRegister, int8_t xn, int numBytesToAccess) {
     uint64_t imm12 = get_bits(instruction, 10, 21) >> 10;
+
+    // Check validity of arguments
+    if ((*targetRegister).mode) {
+        if (imm12 > 32760) {
+            return false;
+        }
+    } else {
+        if (imm12 > 16380) {
+            return false;
+        }
+    }
+
     int64_t address = (*generalPurposeRegisters[xn]).val + imm12;
 
     for (int i = 0; i < numBytesToAccess; i++) {
         memory[address + i] = (*targetRegister).val >> (i * 8);
     }
+    return true;
 }
 
-void pre_indexed(uint32_t instruction, GeneralPurposeRegister *targetRegister, int8_t xn, int numBytesToAccess) {
+bool pre_indexed(uint32_t instruction, GeneralPurposeRegister *targetRegister, int8_t xn, int numBytesToAccess) {
     // Calculates value of address
     int32_t simm9 = get_bits(instruction, 12, 20) >> 12;
     int64_t value = (*generalPurposeRegisters[xn]).val;
@@ -52,10 +66,12 @@ void pre_indexed(uint32_t instruction, GeneralPurposeRegister *targetRegister, i
         // Access using updated value
         memory[address + i] = (*targetRegister).val >> (i * 8);
     }
+
+    return true;
 }
 
 
-void post_indexed(uint32_t instruction, GeneralPurposeRegister *targetRegister, int8_t xn, int numBytesToAccess) {
+bool post_indexed(uint32_t instruction, GeneralPurposeRegister *targetRegister, int8_t xn, int numBytesToAccess) {
     // Calculates value of address
     int32_t simm9 = get_bits(instruction, 12, 20) >> 12;
     int64_t value = (*generalPurposeRegisters[xn]).val;
@@ -69,11 +85,18 @@ void post_indexed(uint32_t instruction, GeneralPurposeRegister *targetRegister, 
         // Access using original value
         memory[value + i] = (*targetRegister).val >> (i * 8);
     }
+
+    return true;
 }
 
-void registerOffset(uint32_t instruction, GeneralPurposeRegister *targetRegister, int8_t xn, int numBytesToAccess) {
+bool registerOffset(uint32_t instruction, GeneralPurposeRegister *targetRegister, int8_t xn, int numBytesToAccess) {
     // Add the two values of registers
     int8_t xm = get_bits(instruction, 16, 20) >> 16;
+
+    if (!(*generalPurposeRegisters[xm]).mode) {
+        return false;
+    }
+
     int64_t address = (*generalPurposeRegisters[xm]).val + (*generalPurposeRegisters[xn]).val;
 
     // Access this address in memory and write this to target register
@@ -81,9 +104,11 @@ void registerOffset(uint32_t instruction, GeneralPurposeRegister *targetRegister
         // Access using original value
         memory[address + i] = (*targetRegister).val >> (i * 8);
     }
+
+    return true;
 }
 
-void singleDTI(uint32_t instruction) {
+bool singleDTI(uint32_t instruction) {
     // get target register
     uint32_t target = get_bits(instruction,0, 4);
     GeneralPurposeRegister *targetRegister = generalPurposeRegisters[target];
@@ -95,27 +120,27 @@ void singleDTI(uint32_t instruction) {
         numBytesToAccess = 4;
     }
 
+    // Modify PC
+    programCounter += 1;
+
     // Determine type of data transfer
     // It is a load literal
     if (get_bits(instruction, 31, 31) == 0) {
-        load_literal(instruction, targetRegister, numBytesToAccess);
+        return load_literal(instruction, targetRegister, numBytesToAccess);
     }
         // It is a single data transfer
     else {
         int8_t xn = get_bits(instruction, 5, 9) >> 5;
         if (get_bits(instruction, 24, 24) != 0) { // U = 1
-            unsigned_immediate_offset(instruction, targetRegister, xn, numBytesToAccess);
+            return unsigned_immediate_offset(instruction, targetRegister, xn, numBytesToAccess);
         } else if (get_bits(instruction, 22, 22) != 0) { // L = 1
-            registerOffset(instruction, targetRegister, xn, numBytesToAccess);
+            return registerOffset(instruction, targetRegister, xn, numBytesToAccess);
         } else {
             if (get_bits(instruction, 11, 11) == 1) {
-                pre_indexed(instruction, targetRegister, xn, numBytesToAccess);
+                return pre_indexed(instruction, targetRegister, xn, numBytesToAccess);
             } else {
-                post_indexed(instruction, targetRegister, xn, numBytesToAccess);
+                return post_indexed(instruction, targetRegister, xn, numBytesToAccess);
             }
         }
     }
-
-    // Modify PC
-    programCounter += 1;
 }
