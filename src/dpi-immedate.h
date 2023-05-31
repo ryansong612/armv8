02651aeb1adaa6557dc6c7c209250317f74f506a
Song, Ryan <ryan.song22@imperial.ac.uk>
2023-05-31 16:13:10 +0000

    Manually merged all files for testing

---
+++ src/dpi-immedate.h
@@ -0,0 +1,18 @@
+#include <stdint.h>
+
+#ifndef ARMV8_32_DPI_H
+#define ARMV8_32_DPI_H
+
+#define ADD 0b00
+#define ADDS 0b01
+#define SUB 0b10
+#define SUBS 0b11
+#define MOVZ 0b00
+#define MOVN 0b10
+#define MOVK 0b11
+
+bool execute_DPIImmediate(uint32_t instruction);
+void arithmetic_helper_64(GeneralPurposeRegister *rd, uint32_t instruction, int64_t rn_val, uint32_t op2);
+void arithmetic_helper_32(GeneralPurposeRegister *rd, uint32_t instruction, int32_t rn_val, uint32_t op2);
+
+#endif //ARMV8_32_DPI_H
---
+++ src/dpi-register.h
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+#define MULADD 0
+#define MULSUB 1
+
+#ifndef ARMV8_32_DPI_REGISTER_DPI_REGISTER_H
+#define ARMV8_32_DPI_REGISTER_DPI_REGISTER_H
+
+void execute_DPIRegister(uint32_t instruction);
+
+#endif //ARMV8_32_DPI_REGISTER_DPI_REGISTER_H
