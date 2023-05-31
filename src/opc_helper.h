eed92378830a839abb4c6e8caac4f514c6821777
Lee, Chris <chris.lee22@imperial.ac.uk>
2023-05-31 15:04:21 +0000

    delete redundunt files

--- src/opc_helper.h
+++
@@ -1,7 +0,0 @@
-#ifndef ARMV8_32_DPI_REGISTER_OPC_HELPER_H
-#define ARMV8_32_DPI_REGISTER_OPC_HELPER_H
-
-void arithmetic_helper_64(GeneralPurposeRegister *rd, uint32_t instruction, int64_t rn_val, uint32_t op2);
-void arithmetic_helper_32(GeneralPurposeRegister *rd, uint32_t instruction, int64_t rn_val, uint32_t op2);
-
-#endif //ARMV8_32_DPI_REGISTER_OPC_HELPER_H
