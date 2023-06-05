3584735eb54586fa8ca0fa3b9bb5e4a4a9879d85
ryansong612 <ryansong612@gmail.com>
2023-06-05 20:25:10 +0100

    Finished .s file reader in assemble.c

--- src/assemble.c
+++ src/assemble.c
@@ -1,7 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define MAX_LINE_LENGTH 256
+
+void read_file(char *dst) {
+    FILE *input;
+    char line[MAX_LINE_LENGTH];
+
+    input = fopen(dst, "r");
+
+    if (input == NULL) {
+        perror("File does not exist.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(line, MAX_LINE_LENGTH, input) != NULL) {
+        printf("%s\n", line);
+    }
+
+    fclose(input);
+}
+
 int main(int argc, char **argv) {
-  printf("Hello\n");
+  read_file(argv[1]);
   return EXIT_SUCCESS;
 }
